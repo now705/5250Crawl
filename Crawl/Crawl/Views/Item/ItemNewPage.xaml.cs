148a03c7153a8355dce7c01e4c50f7ba26e11262
Mike Koenig <mikekoenig@live.com>
2019-01-14 18:24:01 -0800

    Changed Item to Data in New Page and Item New Page

--- Crawl/Crawl/Views/Item/ItemNewPage.xaml.cs
+++ Crawl/Crawl/Views/Item/ItemNewPage.xaml.cs
@@ -11,13 +11,13 @@ namespace Crawl.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemNewPage : ContentPage
     {
-        public Item Item { get; set; }
+        public Item Data { get; set; }
 
         public ItemNewPage()
         {
             InitializeComponent();
 
-            Item = new Item
+            Data = new Item
             {
                 Text = "Item name",
                 Description = "This is an item description."
@@ -28,7 +28,7 @@ namespace Crawl.Views
 
         async void Save_Clicked(object sender, EventArgs e)
         {
-            MessagingCenter.Send(this, "AddItem", Item);
+            MessagingCenter.Send(this, "AddItem", Data);
             await Navigation.PopModalAsync();
         }
 
