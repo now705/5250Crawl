541edf8e437376c4019b9ea7a0adf70d509abb20
Mike Koenig <mikekoenig@live.com>
2019-02-03 18:49:35 -0800

    Unit Test Project Configured, and First Example

---
+++ UnitTests/GameEngine/HelperEngineUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.GameEngine
+{
+    [TestFixture]
+    public class HelperEngineUnitTests
+    {
+        [Test]
+        public void RollDice_Roll_1_Dice_10_Should_Pass()
+        {
+            // Arrange
+            var Roll = 1;
+            var Dice = 10;
+            var Expected = 1;
+
+            // Act
+            var Actual = Crawl.GameEngine.HelperEngine.RollDice(Roll, Dice);
+            
+            // Assert
+            Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
+        }
+    }
+}
