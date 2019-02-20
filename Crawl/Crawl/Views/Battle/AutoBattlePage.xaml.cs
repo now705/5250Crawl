b9a00f298ea07be87354eb4fe43c0915d566c462
Mike Koenig <mikekoenig@live.com>
2019-02-20 14:04:52 -0800

    Prep for Class

--- Crawl/Crawl/Views/Battle/AutoBattlePage.xaml.cs
+++ Crawl/Crawl/Views/Battle/AutoBattlePage.xaml.cs
@@ -19,32 +19,7 @@ namespace Crawl.Views.Battle
         {
             // Can create a new battle engine...
             var myEngine = new AutoBattleEngine();
-
-            var result = myEngine.RunAutoBattle();
-
-            if (result == false)
-            {
-                await DisplayAlert("Error", "No Characters Avaialbe", "OK");
-                return;
-            }
-
-            if (myEngine.GetRoundsValue() < 1)
-            {
-                await DisplayAlert("Error", "No Rounds Fought", "OK");
-                return;
-            }
-
-            var myResult = myEngine.GetResultsOutput();
-            var myScore = myEngine.GetScoreValue();
-
-            var outputString = "Battle Over! Score " + myScore.ToString();
-
-            var action = await DisplayActionSheet(outputString, "Cancel", null, "View Score");
-            if (action == "View Score")
-            {
-                var myScoreObject = myEngine.GetScoreObject();
-                await Navigation.PushAsync(new ScoreDetailPage(new ScoreDetailViewModel(myScoreObject)));
-            }
+            
         }
     }
 }
