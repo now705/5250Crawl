1a264fe836b0f8286507b97061a03e895c7a2bfa
Mike Koenig <mikekoenig@live.com>
2019-02-26 11:20:02 -0800

    Update URL

--- Crawl/Crawl/Models/WebGlobals.cs
+++ Crawl/Crawl/Models/WebGlobals.cs
@@ -3,9 +3,9 @@
     //Global Variables
     public static class WebGlobals
     {
-       
+
         // URL for the web site
-        public const string WebSiteAPIURL = "ABC";
+        public const string WebSiteAPIURL = "https://itemgetpost.azurewebsites.net/API/";
 
         // Error code value used in the json returns
         public const int ErrorResultCode = 1;
