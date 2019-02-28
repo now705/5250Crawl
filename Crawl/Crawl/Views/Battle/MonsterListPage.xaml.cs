2e13a26a0647385c045ce9d8df325397072ec8c8
Mike Koenig <mikekoenig@live.com>
2019-02-28 12:56:47 -0800

    Added text to the MonsterList page, and data binding in the constructor

--- Crawl/Crawl/Views/Battle/MonsterListPage.xaml.cs
+++ Crawl/Crawl/Views/Battle/MonsterListPage.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 using Crawl.GameEngine;
 using Crawl.ViewModels;
+using Crawl.Models;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,10 +14,16 @@ namespace Crawl.Views.Battle
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MonsterListPage : ContentPage
 	{
+        public List<Monster> Datalist = new List<Monster>();
+
 		public MonsterListPage()
 		{
 			InitializeComponent ();
-		}
+
+            Datalist = BattleViewModel.Instance.BattleEngine.MonsterList;
+            BindingContext = Datalist;
+
+        }
 
     }
 }
